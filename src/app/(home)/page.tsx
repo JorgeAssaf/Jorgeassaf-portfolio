d87bbee62a69ef7d4e80a00d00d450e80da8c274
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-14 10:33:36 -0600

    add framer

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,6 +10,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { FADE_UP_ANIMATION_VARIANTS } from '@/constans'
 import { cn } from '@/lib/utils'
 import { AspectRatio } from '@radix-ui/react-aspect-ratio'
 import { motion } from 'framer-motion'
@@ -18,28 +19,53 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
-
   return (
     <main>
       <Hero />
-      <div className='flex flex-col justify-center items-center animate-pulse'>
-        Scroll
-        <ArrowDown className='w-5 h-5 animate-bounce ' />
-      </div>
+      <motion.div
+        initial='hidden'
+        animate='show'
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.15,
+            },
+          },
+        }}
 
-      <section
-        className='grid grid-cols-1 gap-5 ls:grid-cols-2 '>
-        <Card>
-          <CardHeader className='border-b p-0'>
-            <AspectRatio ratio={16 / 9}>
-              <Image
-                src='/netflix-clone.png'
-                alt='Netflix Clone'
-                sizes='(min-width: 640px) 640px, 100vw'
-                fill
-                loading='lazy'
-              />
-              {/* ) : (
+      >
+        <motion.div className='flex flex-col justify-center items-center animate-pulse' variants={FADE_UP_ANIMATION_VARIANTS}>
+          Scroll
+          <ArrowDown className='w-5 h-5 animate-bounce ' />
+        </motion.div>
+      </motion.div>
+
+      <motion.section
+        initial='hidden'
+        animate='show'
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.15,
+            },
+          },
+        }}
+        className='grid grid-cols-1 gap-5 ls:grid-cols-2 '
+      >
+        <motion.div variants={FADE_UP_ANIMATION_VARIANTS}>
+          <Card>
+            <CardHeader className='border-b p-0'>
+              <AspectRatio ratio={16 / 9}>
+                <Image
+                  src='/netflix-clone.png'
+                  alt='Netflix Clone'
+                  sizes='(min-width: 640px) 640px, 100vw'
+                  fill
+                  loading='lazy'
+                />
+                {/* ) : (
                   <div
                     aria-label="Placeholder"
                     role="img"
@@ -52,54 +78,49 @@ export default function Home() {
                     />
                   </div>
                 )} */}
-            </AspectRatio>
-          </CardHeader>
+              </AspectRatio>
+            </CardHeader>
 
-          <CardContent>
-            <CardTitle className='line-clamp-1 my-3'>Recipely</CardTitle>
-            <CardDescription className=''>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              voluptatum.
-
-
-            </CardDescription>
-            <div>
-
-            </div>
-            <CardFooter
-              className={cn('p-0 flex justify-between items-center', 'mt-3')}
-            >
-              <div className='flex items-center gap-5'>
-                <Link
-                  href=''
-                  className={cn(
-                    buttonVariants({
+            <CardContent>
+              <CardTitle className='line-clamp-1 my-3'>Recipely</CardTitle>
+              <CardDescription className=''>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, voluptatum.
+              </CardDescription>
+              <div></div>
+              <CardFooter
+                className={cn('p-0 flex justify-between items-center', 'mt-3')}
+              >
+                <div className='flex items-center gap-5'>
+                  <Link
+                    href=''
+                    className={cn(
+                      buttonVariants({
+                        className: cn(
+                          'font-medium bg-primary hover:bg-primary transition-colors ',
+                        ),
+                      }),
+                    )}
+                  >
+                    View Code
+                  </Link>
+                  <Link
+                    href=''
+                    className={buttonVariants({
+                      variant: 'outline',
                       className: cn(
-                        'font-medium bg-primary hover:bg-primary transition-colors '
-                      )
-                    })
-                  )}
-
-                >
-                  View Code
-                </Link>
-                <Link
-                  href=''
-                  className={buttonVariants({
-                    variant: 'outline',
-                    className: cn(
-                      'font-medium border border-blue-500 transition-colors  '
-                    )
-                  }
-                  )}
-                >
-                  Live Demo
-                </Link>
-              </div>
-            </CardFooter>
-          </CardContent>
-        </Card>
-      </section>
+                        'font-medium border border-blue-500 transition-colors  ',
+                      ),
+                    })}
+                  >
+                    Live Demo
+                  </Link>
+                </div>
+              </CardFooter>
+            </CardContent>
+          </Card>
+        </motion.div>
+      </motion.section>
     </main>
   )
 }
