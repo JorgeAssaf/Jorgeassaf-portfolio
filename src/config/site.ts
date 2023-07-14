a57721274c7d67004ab1287fc5d3a7e3a1396ceb
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-14 13:02:00 -0600

    refactor styles

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,26 +1,23 @@
-
 export type SiteConfig = typeof siteConfig
 
 const links = {
-  twitter: "https://twitter.com/@assafEnrique",
-  github: "https://github.com/sadmann7/portfolio",
-  githubAccount: "https://github.com/jorgeassaf",
-  discord: "https://discord.com/users/sadmann7",
+  twitter: 'https://twitter.com/@assafEnrique',
+  github: 'https://github.com/sadmann7/portfolio',
+  githubAccount: 'https://github.com/jorgeassaf',
+  discord: 'https://discord.com/users/sadmann7',
 }
 
 export const siteConfig = {
-  name: "Assaf",
+  name: 'Assaf',
   description:
-    "Personal portfolio of Jorge Assaf, a software engineer based in Mexico City. ",
-  url: "https://skateshop.sadmn.com",
-  ogImage: "https://skateshop.sadmn.com/opengraph-image.png",
+    'Personal portfolio of Jorge Assaf, a software engineer based in Mexico City. ',
+  url: 'https://skateshop.sadmn.com',
+  ogImage: 'https://skateshop.sadmn.com/opengraph-image.png',
   mainNav: [
-
     {
-      title: "Home",
-      href: "/",
+      title: 'Home',
+      href: '/',
       items: [],
-
     },
     {
       title: 'Projects',
@@ -31,55 +28,50 @@ export const siteConfig = {
       title: 'About',
       href: '/about',
       items: [],
-
     },
-
-
-
   ],
   footerNav: [
     {
-      title: "Social",
+      title: 'Social',
       items: [
         {
-          title: "Twitter",
+          title: 'Twitter',
           href: links.twitter,
           external: true,
         },
         {
-          title: "GitHub",
+          title: 'GitHub',
           href: links.githubAccount,
           external: true,
         },
         {
-          title: "Discord",
+          title: 'Discord',
           href: links.discord,
           external: true,
         },
-
       ],
     },
     {
-      title: "Lofi",
+      title: 'Lofi',
       items: [
         {
-          title: "beats to study to",
-          href: "https://www.youtube.com/watch?v=jfKfPfyJRdk",
+          title: 'beats to study to',
+          href: 'https://www.youtube.com/watch?v=jfKfPfyJRdk',
           external: true,
         },
         {
-          title: "beats to chill to",
-          href: "https://www.youtube.com/watch?v=rUxyKA_-grg",
+          title: 'beats to chill to',
+          href: 'https://www.youtube.com/watch?v=rUxyKA_-grg',
           external: true,
         },
         {
-          title: "a fresh start",
-          href: "https://www.youtube.com/watch?v=rwionZbOryo",
+          title: 'a fresh start',
+          href: 'https://www.youtube.com/watch?v=rwionZbOryo',
           external: true,
         },
         {
-          title: "coffee to go",
-          href: "https://www.youtube.com/watch?v=2gliGzb2_1I",
+          title: 'coffee to go',
+          href: 'https://www.youtube.com/watch?v=2gliGzb2_1I',
           external: true,
         },
       ],
