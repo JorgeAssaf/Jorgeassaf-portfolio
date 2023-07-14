e945fec66313516e10316445ecc0109620c873a7
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-14 10:27:06 -0600

    add framer motion

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,36 +1,61 @@
-import HomeScene from "@/scenes/home-scene"
-
+import { motion } from 'framer-motion'
+import HomeScene from '@/scenes/home-scene'
+import { Button } from './ui/button'
+import { Icons } from './icons'
+import { ArrowDownFromLine } from 'lucide-react'
+import Image from 'next/image'
 
 const Hero = () => {
-
+  const FADE_DOWN_ANIMATION_VARIANTS = {
+    hidden: { opacity: 0, y: -10 },
+    show: { opacity: 1, y: 0, transition: { type: "spring" } },
+  };
   return (
-    <div className='my-14 md:my-10 flex items-center '>
-      <div className="max-w-[34rem]">
-        <h1 className='text-4xl md:text-6xl font-bold'>Hi, I’m Jorge Assaf.</h1>
-        <h2 className='text-blue-500 md:text-4xl mt-3 font-semibold text-2xl'>
+    <motion.div
+      initial="hidden"
+      animate="show"
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
+          },
+        },
+      }}
+      className='my-14 md:my-10 flex items-center '>
+      <div className='max-w-[34rem]'>
+        <motion.h1 variants={FADE_DOWN_ANIMATION_VARIANTS} className='text-4xl md:text-6xl font-bold'>Hi, I’m Jorge Assaf.</motion.h1>
+        <motion.h2 variants={FADE_DOWN_ANIMATION_VARIANTS} className='decoration-wavy underline-offset-1 underline text-primary md:text-4xl mt-3 font-semibold text-2xl'>
           Front-end Developer.
-        </h2>
-        <p className='text-gray-500 mt-3 md:text-xl text-base'>
-          I’m a Front-end Developer based in Mexico City, Mexico. I have a passion for web development and love to create for web and mobile devices.
-        </p>
-        <div className='mt-5'>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-            Get in touch
-          </button>
-          <button className='ml-3 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
-            See my work
-          </button>
-
-
-        </div>
-
+        </motion.h2>
+        <motion.p variants={FADE_DOWN_ANIMATION_VARIANTS} className='text-gray-500 mt-3 md:text-xl text-base'>
+          I’m a Front-end Developer based in Mexico City, Mexico. I have a
+          passion for web development and love to create for web and mobile
+          devices.
+        </motion.p>
+        <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS} className='mt-5 flex gap-5'>
+          <Button className='bg-primary'>
+            <Icons.gitHub className='w-5 h-5' />
+          </Button>
+          <Button className='bg-primary'>
+            <Icons.linkedIn className='w-5 h-5' />
+          </Button>
+          <Button variant='outline'>Resumen</Button>
+        </motion.div>
       </div>
-      <div className="max-w-lg mx-auto hidden lg:flex">
+      <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS} className='max-w-lg h-full mx-auto hidden lg:flex'>
         {/* <HomeScene /> */}
 
-
-      </div>
-    </div>
+        <Image
+          src='/Vector.png'
+          alt='image'
+          className='h-72 '
+          width={320}
+          height={500}
+        />
+      </motion.div>
+    </motion.div>
   )
 }
 
--- src/components/layout/site-header.tsx
+++ src/components/layout/site-header.tsx
@@ -1,15 +1,31 @@
+'use client'
+import { motion } from 'framer-motion'
+
+import { FADE_UP_ANIMATION_VARIANTS } from '@/constans'
 import { siteConfig } from '@/config/site'
 
 import MainNav from './main-nav'
 import MobileNav from './mobile-nav'
+
 const SiteHeader = () => {
   return (
-    <header className='sticky top-0 z-40 w-full  bg-background'>
-      <div className='container py-5 '>
+    <motion.header initial="hidden"
+      animate="show"
+      viewport={{ once: true }}
+      variants={{
+        hidden: {},
+        show: {
+          transition: {
+            staggerChildren: 0.15,
+          },
+        },
+      }}
+      className='sticky top-0 z-40 w-full bg-background/50 backdrop-blur-md'>
+      <motion.div variants={FADE_UP_ANIMATION_VARIANTS} className='container py-5 '>
         <MainNav items={siteConfig.mainNav} />
         <MobileNav items={siteConfig.mainNav} />
-      </div>
-    </header>
+      </motion.div>
+    </motion.header>
   )
 }
 
