578493fbe0b9978f8c62a7766cfe0c45f30e9e3d
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-14 10:24:27 -0600

    add suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' suppressHydrationWarning>
       <body
         className={cn(
           'min-h-screen bg-background antialiased ',
