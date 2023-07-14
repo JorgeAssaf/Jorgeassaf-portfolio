63325e33569429fbf94798a506e7842bcdf81bb0
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-14 11:47:32 -0600

    webp and avif formats

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    formats: ['image/webp', 'image/avif'],
+  }
+
+}
 
 module.exports = nextConfig
