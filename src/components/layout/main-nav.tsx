61bd767272eb543bc644a566db38e7688f7d6bbd
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-14 13:01:43 -0600

    add new color and pathname

--- src/components/layout/main-nav.tsx
+++ src/components/layout/main-nav.tsx
@@ -1,13 +1,17 @@
+'use client'
 import { MainNavItem } from '@/app/types/site'
-import { FC } from 'react'
+import { FC, forwardRef } from 'react'
 import { Icons } from '../icons'
 import { siteConfig } from '@/config/site'
 import Link from 'next/link'
 import { ThemeToggle } from './theme-toggle'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
 interface MainNavProps {
   items?: MainNavItem[]
 }
 const MainNav: FC<MainNavProps> = ({ items }) => {
+  const pathname = usePathname()
   return (
     <>
       <div className='hidden h-20 items-center md:flex justify-between'>
@@ -21,15 +25,16 @@ const MainNav: FC<MainNavProps> = ({ items }) => {
           </p>
         </Link>
 
-        <div className='flex gap-6'>
+        <div className='flex gap-6 items-center'>
           {items?.map((item) => (
-            <Link
+            <MenuLink
+              pathname={pathname}
               key={item.title}
-              href={`${item.href}`}
-              className='flex items-center space-x-2 text-base  transition-colors font-medium'
+              href={item.href!}
+              className='text-lg font-medium'
             >
-              <span>{item.title}</span>
-            </Link>
+              {item.title}
+            </MenuLink>
           ))}
           <ThemeToggle />
         </div>
@@ -39,3 +44,33 @@ const MainNav: FC<MainNavProps> = ({ items }) => {
 }
 
 export default MainNav
+interface MenuLinkProps {
+  children?: React.ReactNode
+  href: string
+  disabled?: boolean
+  pathname: string
+  className?: string
+}
+
+function MenuLink({
+  children,
+  href,
+  disabled,
+  pathname,
+  className,
+}: MenuLinkProps) {
+  return (
+    <Link
+      href={`${href}`}
+      className={cn(
+        "text-foreground transition-colors hover:text-primary-hover",
+        pathname === href && "text-primary-hover",
+        disabled && "pointer-events-none opacity-60",
+        className
+      )}
+
+    >
+      {children}
+    </Link>
+  )
+}
