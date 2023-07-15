58ab1732f7f7664885211218654250ecde9837f6
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-14 22:38:19 -0600

    add projects component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,23 +1,13 @@
 'use client'
-import Image from 'next/image'
-import Link from 'next/link'
-import { AspectRatio } from '@/components/ui/aspect-ratio'
 import Hero from '@/components/hero'
-import { buttonVariants } from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
+
 import { FADE_DOWN_ANIMATION_VARIANTS } from '@/constans'
-import { cn } from '@/lib/utils'
 import { motion } from 'framer-motion'
 import { ArrowDown } from 'lucide-react'
+import Projects from '@/components/projects'
 
 export default function Home() {
+
   return (
     <main>
       <Hero />
@@ -56,72 +46,8 @@ export default function Home() {
         className='grid grid-cols-1 gap-5 ls:grid-cols-2 '
       >
         <motion.div variants={FADE_DOWN_ANIMATION_VARIANTS}>
-          <Card>
-            <CardHeader className='border-b bg-card p-0'>
-              <AspectRatio ratio={16 / 9}>
-                <Image
-                  src='/netflix-clone.png'
-                  alt='Netflix Clone'
-                  sizes='(min-width: 640px) 640px, 100vw'
-                  fill
-                  loading='lazy'
-                />
-                {/* ) : (
-                  <div
-                    aria-label="Placeholder"
-                    role="img"
-                    aria-roledescription="placeholder"
-                    className="flex h-full w-full items-center justify-center bg-secondary"
-                  >
-                    <Icons.placeholder
-                      className="h-9 w-9 text-muted-foreground"
-                      aria-hidden="true"
-                    />
-                  </div>
-                )} */}
-              </AspectRatio>
-            </CardHeader>
+          <Projects />
 
-            <CardContent>
-              <CardTitle className='line-clamp-1 text-2xl my-3'>
-                Recipely
-              </CardTitle>
-              <CardDescription className='text-base '>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quisquam, voluptatum.
-              </CardDescription>
-              <div></div>
-              <CardFooter
-                className={cn('p-0 flex justify-between items-center', 'mt-3')}
-              >
-                <div className='flex items-center gap-5'>
-                  <Link
-                    href=''
-                    className={cn(
-                      buttonVariants({
-                        className: cn(
-                          'font-medium bg-primary  transition-colors ',
-                        ),
-                      }),
-                    )}
-                  >
-                    View Code
-                  </Link>
-                  <Link
-                    href=''
-                    className={buttonVariants({
-                      variant: 'outline',
-                      className: cn(
-                        'font-medium border border-primary transition-colors  ',
-                      ),
-                    })}
-                  >
-                    Live Demo
-                  </Link>
-                </div>
-              </CardFooter>
-            </CardContent>
-          </Card>
         </motion.div>
       </motion.section>
     </main>
---
+++ src/components/projects.tsx
@@ -0,0 +1,104 @@
+import { getProjects } from '@/sanity/sanity-utils'
+import { buttonVariants } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import Image from 'next/image'
+import Link from 'next/link'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+import { cn } from '@/lib/utils'
+import { Icons } from './icons'
+import { PortableText } from '@portabletext/react'
+
+const Projects = async () => {
+  const projects = await getProjects()
+  console.log(projects)
+  return (
+    <>
+      {projects.map((project) => (
+        <Card key={project._id}>
+          <CardHeader className='border-b bg-card p-0'>
+            <AspectRatio ratio={16 / 9}>
+              project.image ? (
+              <Image
+                src={project.image}
+                alt={project.name}
+                sizes='(min-width: 640px) 640px, 100vw'
+                fill
+                loading='lazy'
+              />
+              ) : (
+              <div
+                aria-label='Placeholder'
+                role='img'
+                aria-roledescription='placeholder'
+              >
+                <Icons.placeholder
+                  className='h-9 w-9 text-muted-foreground'
+                  aria-hidden='true'
+                />
+              </div>
+              )
+            </AspectRatio>
+          </CardHeader>
+
+          <CardContent>
+            <CardTitle className='line-clamp-1 text-2xl my-3'>
+              {project.name}
+            </CardTitle>
+
+            <PortableText value={project.content} />
+
+            <div>
+              {project.technologies.map((technologie) => (
+                <Image key={technologie.name} src={technologie.image} alt={technologie.name} width={24} height={24} />
+              ))}
+
+
+            </div>
+            <CardFooter
+              className={cn('p-0 flex justify-between items-center', 'mt-3')}
+            >
+              <div className='flex items-center gap-5'>
+                <Link
+                  href={project.github}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className={cn(
+                    buttonVariants({
+                      className: cn(
+                        'font-medium bg-primary  transition-colors ',
+                      ),
+                    }),
+                  )}
+                >
+                  View Code
+                </Link>
+                <Link
+                  href={project.url}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className={buttonVariants({
+                    variant: 'outline',
+                    className: cn(
+                      'font-medium border border-primary transition-colors  ',
+                    ),
+                  })}
+                >
+                  Live Demo
+                </Link>
+              </div>
+            </CardFooter>
+          </CardContent>
+        </Card>
+      ))}
+    </>
+  )
+}
+
+export default Projects
