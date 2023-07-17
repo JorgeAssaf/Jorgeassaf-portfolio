823469fcff1bdd315084b5ed9208d6b108ab4a6d
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-17 11:25:25 -0600

    style

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,7 @@
-import Hero from "@/components/hero"
-import Projects from "@/components/projects"
-
+import Hero from '@/components/hero'
+import Projects from '@/components/projects'
 
 export default function Home() {
-
   return (
     <main>
       <Hero />
