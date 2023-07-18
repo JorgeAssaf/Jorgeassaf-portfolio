440845410cf4cce8395abd2b13400240f3dad106
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-18 15:39:22 -0600

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/og.jpg`],
+    images: [`${siteConfig.url}/opengraph-image.png`],
     creator: '@AssafEnrique',
   },
   icons: {
