97f7960e5f9e22cf0613fff8d951cf8206c24beb
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-19 14:39:07 -0600

    add post and categories via sanity

--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -1,37 +1,23 @@
 import { Header } from '@/components/header'
 import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import { client } from '@/lib/sanity'
+import { CategoryQuery, PostQuery } from '@/utils/querys'
 import { PortableText } from '@portabletext/react'
 
-const blogCategories = [
-  {
-    name: 'All',
-    value: 'all',
-  },
-  {
-    name: 'Web Development',
-    value: 'web-development',
-  },
-  {
-    name: 'Mobile Development',
-    value: 'mobile-development',
-  },
-  {
-    name: 'UI/UX Design',
-    value: 'ui-ux-design',
-  },
 
-  {
-    name: 'DevOps',
-    value: 'devops',
-  },
-]
 
 const BlogPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined }
 }) => {
+
+  const blogCategories = await client.fetch(CategoryQuery)
+  const blog = await client.fetch(PostQuery)
+  const [posts, categories] = await Promise.all([blog, blogCategories])
   console.log(searchParams)
+  console.log(posts, categories);
   return (
     <>
       <Header
@@ -41,44 +27,41 @@ const BlogPage = async ({
       />
       <div className='md:grid grid-rows-1 grid-flow-col gap-4 flex flex-col '>
         <div className='row-span-1 flex gap-2 md:flex-col flex-row flex-wrap md:justify-start justify-between'>
-          {blogCategories.map((category) => (
-            <div key={category.value} className='flex '>
+          {categories.map((category) => (
+            <div key={category.title} className='flex '>
               <Button className='' variant='outline' size='sm'>
-                {category.name}
+                {category.title}
               </Button>
             </div>
           ))}
         </div>
         <div className=' flex flex-col max-w-3xl'>
-          <div className='md:mt-2 mt-0'>
-            <div className=''>
-              <h1 className='text-3xl font-bold'>Mobile Development</h1>
-            </div>
-          </div>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptatum, voluptate, quibusdam, quia voluptas quod quos
-              voluptatibus quae doloribus quidem fugiat. Quisquam voluptatum,
-              voluptate, quibusdam, quia voluptas quod quos voluptatibus quae
-              doloribus quidem fugiat. Quisquam voluptatum, voluptate,
-              quibusdam,
-            </p>
-          </div>
+          {
+            posts.map((post) => (
+              <Card key={post._id}>
+                <div className='md:mt-2 mt-0'>
+                  <div className=''>
+                    <h1 className='text-3xl font-bold'>{post.title}</h1>
+                  </div>
+                  <div className='flex gap-4'>
+                    <span>
+                      {new Date(post._createdAt).toLocaleDateString()}
+                    </span>
+                    <span>
+                      {post.categories.map((category) => (
+                        <span key={category.title}>{category.title}</span>
+                      ))}
+                    </span>
+                  </div>
+                </div>
+                <div>
+                  <PortableText value={post.body} />
+                </div>
+              </Card>
+            ))
+          }
 
-          <div className='md:mt-2 mt-0'>
-            <h1 className='text-3xl font-bold'>Mobile Development</h1>
-          </div>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptatum, voluptate, quibusdam, quia voluptas quod quos
-              voluptatibus quae doloribus quidem fugiat. Quisquam voluptatum,
-              voluptate, quibusdam, quia voluptas quod quos voluptatibus quae
-              doloribus quidem fugiat. Quisquam voluptatum, voluptate,
-              quibusdam,
-            </p>
-          </div>
+
         </div>
       </div>
     </>
