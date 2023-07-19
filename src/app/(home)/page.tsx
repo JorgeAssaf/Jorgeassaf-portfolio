f9abb3c7866b9d573a2a1d935335ee13f4074a34
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-19 14:38:13 -0600

    delete about in home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -14,25 +14,7 @@ export default async function Home() {
       <Scroll />
       <Header title='Projects' />
       <Projects projects={projects} />
-      <Header title='Hey Here' className='mt-30' />
-      <div>
-        <p className='mb-5'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Et odit quos
-          vel impedit id quam, obcaecati laborum provident ipsum beatae cum
-          voluptates! Consequuntur, ipsa labore asperiores natus libero qui
-          ipsum!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. A corporis
-          natus ratione voluptatum voluptate odit est quisquam consequatur,
-          fugiat, provident excepturi culpa sed, facilis ex doloremque dicta
-          consectetur explicabo distinctio. Placeat officiis sit reiciendis
-          numquam rerum repudiandae, necessitatibus incidunt voluptates expedita
-          iste maiores consectetur cumque minima. Cumque voluptatem vitae sunt
-          rem porro adipisci voluptate iste saepe sed consectetur. Soluta, enim.
-        </p>
-        <button>Click me</button>
-      </div>
+
     </main>
   )
 }
