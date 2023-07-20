6a39b6fce431222ffcdb949edbabacf43bd3da01
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-20 10:11:58 -0600

    add metadata

--- src/app/(home)/projects/page.tsx
+++ src/app/(home)/projects/page.tsx
@@ -2,6 +2,13 @@ import { Header } from '@/components/header'
 import Projects from '@/components/projects'
 import { client } from '@/lib/sanity'
 import { projectsQuery } from '@/utils/querys'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Projects',
+  description:
+    'Here you can see my latest projects and what technologies they are made with',
+}
 
 const getProjects = async () => {
   const projects = (await client.fetch<Projects[]>(projectsQuery)) ?? []
