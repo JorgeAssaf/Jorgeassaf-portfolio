c4be13c3e8927347213700292f4b48ee18df6266
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-19 22:57:42 -0600

    add getPostsByCategory

--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -1,21 +1,30 @@
 import { Category, Post } from '@/app/types/sanity'
+import CategoryButtons from '@/components/category-buttons'
 import { Header } from '@/components/header'
-import { Button } from '@/components/ui/button'
+import { Badge } from '@/components/ui/badge'
 import { Card } from '@/components/ui/card'
 import { client } from '@/lib/sanity'
+import { cn } from '@/lib/utils'
 import { CategoryQuery, PostQuery } from '@/utils/querys'
+import { slugify } from '@/utils/utils'
 
+const getPostsByCategory = async (category: string) => {
+  const posts = await client.fetch(PostQuery)
+  if (!category) return posts
 
+  return posts.filter((post: Post) =>
+    post.categories.some((c: Category) => slugify(c.title) === category),
+  )
+}
 
 const BlogPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined }
 }) => {
-
   const blogCategories = await client.fetch(CategoryQuery)
-  const blog = await client.fetch(PostQuery)
-  const [posts, categories] = await Promise.all([blog, blogCategories])
+  const blogPost = await getPostsByCategory(searchParams.category as string)
+  const [posts, categories] = await Promise.all([blogPost, blogCategories])
   return (
     <>
       <Header
@@ -25,38 +34,38 @@ const BlogPage = async ({
       />
       <div className='md:grid grid-rows-1 grid-flow-col gap-4 flex flex-col '>
         <div className='row-span-1 flex gap-2 md:flex-col flex-row flex-wrap md:justify-start justify-between'>
-          {categories.map((category: Category) => (
-            <div key={category.title} className='flex '>
-              <Button className='' variant='outline' size='sm'>
-                {category.title}
-              </Button>
-            </div>
-          ))}
+          <CategoryButtons categories={categories} />
         </div>
         <div className=' flex flex-col max-w-3xl'>
-          {
+          {posts.length > 0 ? (
             posts.map((post: Post) => (
               <Card key={post._id}>
                 <div className='md:mt-2 mt-0'>
-
                   <h2 className='text-3xl font-bold'>{post.title}</h2>
 
                   <div className='flex gap-4'>
                     <span>
                       {new Date(post._createdAt).toLocaleDateString()}
                     </span>
-                    <span>
+                    <div className='flex gap-3'>
                       {post.categories.map((category) => (
-                        <span key={category.title}>{category.title}</span>
+                        <Badge
+                          className={cn(
+                            'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground',
+                          )}
+                          key={category.title}
+                        >
+                          {category.title}
+                        </Badge>
                       ))}
-                    </span>
+                    </div>
                   </div>
                 </div>
               </Card>
             ))
-          }
-
-
+          ) : (
+            <h2>No posts found</h2>
+          )}
         </div>
       </div>
     </>
