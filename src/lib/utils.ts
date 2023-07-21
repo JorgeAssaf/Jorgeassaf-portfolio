e27e2f610e9c1e0d8bff20ac3de203ad569932ef
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-20 20:34:59 -0600

    update utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,12 +7,22 @@ export function cn(...inputs: ClassValue[]) {
 
 export const slugify = (text: string) => {
   return text
-    .toString()
-    .toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/[^\w-]+/g, '') // Remove all non-word chars
-    .replace(/--+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, ''); // Trim - from end of text
+    .normalize('NFKD') // split accented characters into their base characters and diacritical marks
+    .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+    .trim() // trim leading or trailing whitespace
+    .toLowerCase() // convert to lowercase
+    .replace(/[^a-z0-9 -]/g, '') // remove non-alphanumeric characters
+    .replace(/\s+/g, '-') // replace spaces with hyphens
+    .replace(/-+/g, '-') // remove consecutive hyphens
 }
 
+export const deslugify = (text: string) => {
+  return text
+    .toString()
+    .replace(/-/g, ' ')
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase())
+    .replace(/^[a-z]/g, (m) => m.toUpperCase())
+    .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()))
+}
