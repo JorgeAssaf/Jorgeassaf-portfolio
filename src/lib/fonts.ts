1aa4cbe84fc5dfe9e38e03870bc30cd68d4be177
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-21 13:11:13 -0600

    preload fonts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,11 +1,17 @@
 import localFont from 'next/font/local'
 import { JetBrains_Mono as FontMono } from 'next/font/google'
 
-export const Satoshi = localFont({ src: '../fonts/Satoshi-Variable.ttf' })
+export const Satoshi = localFont({
+  src: '../fonts/Satoshi-Variable.ttf',
+  preload: true,
+  display: 'swap',
+  variable: '--font-sans',
+})
 
 export const fontmono = FontMono({
   weight: ['300', '400', '500', '700'],
   variable: '--font-mono',
   display: 'swap',
+  preload: true,
   subsets: ['latin'],
 })
