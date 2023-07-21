66d7e5c160b68ffebea75bbe7bf5d2f4b456b5d8
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-21 13:11:45 -0600

    font variables in tailwindcss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -58,6 +58,7 @@ module.exports = {
         },
       },
       fontFamily: {
+        sans: ['var(--font-sans)', 'ui-sans-serif', 'system-ui'],
         mono: ['var(--font-mono)', 'ui-monospace', 'SFMono-Regular'],
       },
       borderRadius: {
