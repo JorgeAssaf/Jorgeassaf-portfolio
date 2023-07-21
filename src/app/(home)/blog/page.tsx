75cfc5dc941724e2b3279c054fff7803bfcbafe9
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-20 20:34:02 -0600

    refactor and update metadata

--- src/app/(home)/blog/page.tsx
+++ src/app/(home)/blog/page.tsx
@@ -1,4 +1,3 @@
-import type { Category, Post } from '@/app/types/sanity'
 import CategoryButtons from '@/components/category-buttons'
 import { Header } from '@/components/header'
 import { Badge } from '@/components/ui/badge'
@@ -6,14 +5,16 @@ import { Card } from '@/components/ui/card'
 import { client } from '@/lib/sanity'
 import { cn, slugify } from '@/lib/utils'
 import { CategoryQuery, PostQuery } from '@/utils/querys'
-import type { Metadata } from 'next'
 
+import type { Metadata } from 'next'
+import type { Category, Post } from '@/app/types/sanity'
+import PostCard from '@/components/post-card'
 
 export const metadata: Metadata = {
   title: 'Blog',
-  description: 'Here my last posts about web development, mobile development, ui/ux design, devops etc ',
+  description:
+    'Here my last posts about web development, mobile development, ui/ux design, devops etc ',
 }
-
 
 const getPostsByCategory = async (category: string) => {
   const posts = await client.fetch(PostQuery)
@@ -40,53 +41,41 @@ const BlogPage = async ({
         className='my-10'
       />
 
-      <div className='md:grid grid-rows-1 grid-flow-col gap-5 flex flex-col justify-start '>
-        <div className='row-span-1 flex gap-2 md:flex-col flex-row flex-wrap md:justify-start  md:pr-5 md:border-p-2 pr-0 border-none'>
+      <div className='md:grid grid-cols-[0.5fr,3fr] flex flex-col gap-10'>
+        <div className='flex md:flex-col flex-row flex-wrap  gap-5  '>
           <CategoryButtons categories={categories} />
         </div>
         <div className='flex flex-col gap-5'>
-          <h2 className='text-2xl font-bold capitalize'>
-            {searchParams.category ? (
-              <>{searchParams.category}</>
-            ) : (
-              <> All posts</>
-            )}
+          <h2 className='text-2xl font-bold'>
+            {blogCategories.find(
+              (category: Category) =>
+                slugify(category.title) === searchParams.category,
+            )?.title ?? 'All posts'}
           </h2>
-          <div className=' flex flex-col max-w-xl'>
-            {posts.length > 0 ? (
-              posts.map((post: Post) => (
-                <Card key={post._id}>
-                  <div className='md:mt-2 mt-0'>
-                    <h2 className='text-xl font-bold'>{post.title}</h2>
 
-                    <div className='flex gap-4'>
-                      <span>
-                        {new Date(post._createdAt).getMonth()} months ago
-                      </span>
-                      <div className='flex gap-3'>
-                        {post.categories.map((category) => (
-                          <Badge
-                            className={cn(
-                              'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground',
-                            )}
-                            key={category.title}
-                          >
-                            {category.title}
-                          </Badge>
-                        ))}
-                        {post.author.name}
-                      </div>
-                    </div>
-                  </div>
-                </Card>
-              ))
-            ) : (
-              <h2>No posts found</h2>
-            )}
-          </div>
+          <PostCard posts={posts} />
         </div>
       </div>
     </>
   )
 }
 export default BlogPage
+
+// < div className = 'md:grid grid-rows-1 w-full grid-flow-col gap-5 flex flex-col justify-start ' >
+//       <div className='row-span-1 flex gap-2 md:flex-col flex-row flex-wrap md:justify-start  md:pr-5 md:border-p-2 pr-0 border-none'>
+//
+//       </div>
+//       <div className='grid grid-cols-1 gap-5'>
+//         <div className='flex flex-col gap-5 max-w-xl'>
+//           <h2 className='text-2xl font-bold'>
+//             {blogCategories.find(
+//               (category: Category) =>
+//                 slugify(category.title) === searchParams.category,
+//             )?.title ?? 'All posts'}
+//           </h2>
+
+//           <PostCard posts={posts} />
+//         </div>
+//       </div>
+
+//     </ >
