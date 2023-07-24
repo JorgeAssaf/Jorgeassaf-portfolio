e071439b585f5945299a068b14633013cb0c2652
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-23 20:31:53 -0600

    add metadata

--- src/app/(home)/about/page.tsx
+++ src/app/(home)/about/page.tsx
@@ -1,5 +1,13 @@
 import { Header } from '@/components/header'
 import { Icons } from '@/components/icons'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'About me',
+  description: 'I am a front-end web developer with experience in JavaScript, React, Next.js and Astro. My goal is to become a FullStack programmer. I enjoy creating beautiful and easy to use web applications that connect with users. I am always looking for new opportunities to grow and collaborate on exciting projects.',
+}
+
+
 const AboutPage = () => {
   return (
     <div className='md:w-3/4 mx-auto '>
