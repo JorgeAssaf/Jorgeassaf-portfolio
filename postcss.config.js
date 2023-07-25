700671a7ee89d88c4bee8febe638417a94651996
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-25 11:16:19 -0600

    style: 💄 space

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,10 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+        cssnano: {},
+      }
+      : {}),
   },
 }
