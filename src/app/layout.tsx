d38406b687b5245df1d1162090b5115197b1c390
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-25 15:48:59 -0600

    fix: 🐛 metadataBase default localhost

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import '@/styles/globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL ?? ''),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000'),
   title: {
     default: siteConfig.name,
     template: `%s - ${siteConfig.name}`,
