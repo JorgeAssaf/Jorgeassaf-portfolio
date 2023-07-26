fb69a9ec7f0ada59bf9103053a137954b87259c5
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-25 21:15:26 -0600

    feat: 🎸 force static page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,6 +5,8 @@ import Scroll from '@/components/scroll'
 import { client } from '@/lib/sanity'
 import { projectsQuery } from '@/utils/querys'
 
+export const dynamic = 'force-static'
+
 export default async function Home() {
   const projects = (await client.fetch<Projects[]>(projectsQuery)) ?? []
 
