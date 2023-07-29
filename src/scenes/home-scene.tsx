573aff9441a237c3cba8aa9216ec52e69774c2ac
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-28 22:43:33 -0600

    feat: 🎸 add model dog temporal

--- src/scenes/home-scene.tsx
+++ src/scenes/home-scene.tsx
@@ -1,21 +1,39 @@
 'use client'
+import { useLoader } from '@react-three/fiber'
 
-import { Environment, OrbitControls } from "@react-three/drei"
-import { Canvas, useFrame } from "@react-three/fiber"
-import { useRef, useState } from "react"
+import { Suspense } from 'react'
+import { CameraControls, Environment, Html, OrbitControls, PerspectiveCamera, useProgress } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+
+import { Model } from '@/scenes/Model'
+
+function Loader() {
+  const { progress } = useProgress()
+  return <Html position={[-3, 4, -5]}>{progress} % loaded</Html>
+}
+
 
 const HomeScene = () => {
-
   return (
-    <Canvas className="w-[700px] h-[700px]">
+    <Canvas
 
-      <Environment preset="sunset" />
-      <OrbitControls />
-      <ambientLight intensity={0.1} />
-      <mesh>
-        <sphereGeometry args={[1, 16, 16]} />
-        <meshStandardMaterial color='red' />
-      </mesh>
+      style={{
+        width: 500,
+        height: 500,
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        alignItems: 'center',
+        position: 'relative',
+        top: 70,
+      }}
+    >
+      <Suspense fallback={<Loader />}>
+        <Model position={[0, 0, 0]} scale={[0.6, 0.6, 0.6]} />
+        <PerspectiveCamera position={[4, 3, 5]} makeDefault />
+        <OrbitControls />
+        <Environment preset='sunset' />
+      </Suspense>
     </Canvas>
   )
 }
