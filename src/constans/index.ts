44abc9b7a96d221ffea9163d5d49c7992e7d065a
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-29 22:24:40 -0600

    feat: 🎸 add FADE_LEFT_ANIMATION_VARIANTS

--- src/constans/index.ts
+++ src/constans/index.ts
@@ -1,6 +1,9 @@
-
 export const FADE_DOWN_ANIMATION_VARIANTS = {
   hidden: { opacity: 0, y: -10 },
   show: { opacity: 1, y: 0, transition: { type: 'spring' } },
 }
 
+export const FADE_LEFT_ANIMATION_VARIANTS = {
+  hidden: { opacity: 0, x: -10 },
+  show: { opacity: 1, x: 0, transition: { type: 'spring' } },
+}
