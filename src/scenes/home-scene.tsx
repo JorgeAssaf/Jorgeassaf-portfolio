7e26c561e659ce20abaa98de8ba2f6f47aba6349
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-29 18:44:26 -0600

    refactor: 💡 styles and add ambient light

--- src/scenes/home-scene.tsx
+++ src/scenes/home-scene.tsx
@@ -1,38 +1,38 @@
 'use client'
-import { useLoader } from '@react-three/fiber'
 
 import { Suspense } from 'react'
-import { CameraControls, Environment, Html, OrbitControls, PerspectiveCamera, useProgress } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  useProgress,
+} from '@react-three/drei'
 
 import { Model } from '@/scenes/Model'
 
-function Loader() {
+function LoaderModel() {
   const { progress } = useProgress()
-  return <Html position={[-3, 4, -5]}>{progress} % loaded</Html>
+  return (
+    <Html center position={[-3, 4, -5]}>
+      {progress} % loaded
+    </Html>
+  )
 }
-
 
 const HomeScene = () => {
   return (
     <Canvas
-
       style={{
         width: 500,
         height: 500,
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'row',
-        alignItems: 'center',
-        position: 'relative',
-        top: 70,
       }}
     >
-      <Suspense fallback={<Loader />}>
-        <Model position={[0, 0, 0]} scale={[0.6, 0.6, 0.6]} />
+      <Suspense fallback={<LoaderModel />}>
+        <Model position={[0, -1, 0]} scale={[0.6, 0.6, 0.6]} />
         <PerspectiveCamera position={[4, 3, 5]} makeDefault />
         <OrbitControls />
-        <Environment preset='sunset' />
+        <ambientLight intensity={1.8} />
       </Suspense>
     </Canvas>
   )
