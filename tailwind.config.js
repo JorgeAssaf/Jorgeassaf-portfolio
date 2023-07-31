7fcf0f179f1c0aa00aa3391527024010f2561e1d
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-30 19:53:01 -0600

    feat: 🎸 add tailwindcss-animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,25 +73,12 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: 0 },
         },
-        pulse: {
-          '0%, 100%': { opacity: 1 },
-          '50%': { opacity: 0.5 },
-        },
-        debounce: {
-          '0%, 100%': { opacity: 1 },
-          '50%': { opacity: 0.5 },
-        },
-
-        animation: {
-          'accordion-down': 'accordion-down 0.2s ease-out',
-          'accordion-up': 'accordion-up 0.2s ease-out',
-          'animate-pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-          'animate-debounce': 'bounce 1s infinite;',
-        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
-    plugins: [
-      require('@tailwindcss/typography'),
-    ],
   },
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
 }
