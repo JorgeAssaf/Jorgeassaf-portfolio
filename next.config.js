ce498136397ad0608a359fc66ef9d89355d7d063
Jorge Assaf <jorgeassaf160@gmail.com>
2023-07-30 20:11:37 -0600

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
   images: {
     formats: ['image/webp'],
     domains: ['cdn.sanity.io'],
   },
   transpilePackages: ['three'],
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.optimization.splitChunks.cacheGroups.commons.minChunks = 2
+    }
+    return config
+  }
+
 }
 
 module.exports = nextConfig
