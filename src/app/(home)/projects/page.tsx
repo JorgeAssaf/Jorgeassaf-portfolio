869910ebef4e1e7175a622467c42302c121284ce
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-03 23:30:08 -0600

    :recycle: Refactor ProjectsPage component to include category buttons and update page header

--- src/app/(home)/projects/page.tsx
+++ src/app/(home)/projects/page.tsx
@@ -1,9 +1,13 @@
-import { Header } from '@/components/header'
-import Projects from '@/components/projects'
-import { client } from '@/lib/sanity'
-import { projectsQuery } from '@/utils/querys'
-import type { Projects as ProjectsType } from '@/app/types/sanity'
 import type { Metadata } from 'next'
+import { FADE_DOWN_ANIMATION_VARIANTS } from '@/constans'
+import { projectsQuery } from '@/utils/querys'
+
+import { client } from '@/lib/sanity'
+import CategoryButtons from '@/components/category-buttons'
+import { FramerDiv } from '@/components/framer'
+import { PageHeader } from '@/components/page-header'
+import Projects from '@/components/projects'
+import type { Projects as ProjectsType } from '@/app/types/sanity'
 
 export const metadata: Metadata = {
   title: 'Projects',
@@ -11,16 +15,37 @@ export const metadata: Metadata = {
     'Here you can see my latest projects and what technologies they are made with',
 }
 
+const categories = [
+  { title: 'All' },
+  { title: 'Frontend' },
+  { title: 'Backend' },
+  { title: 'Mobile' },
+  { title: 'Fullstack' },
+]
+
 const ProjectsPage = async () => {
   const projects = (await client.fetch<ProjectsType[]>(projectsQuery)) ?? []
   return (
     <section>
-      <Header
+      <PageHeader
         page
         title='Projects'
         description='Here you can see my last projects'
       />
-      <Projects projects={projects} />
+      <div className='flex flex-col gap-10'>
+        <FramerDiv
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+          className='flex flex-wrap gap-2'
+        >
+          {/* <CategoryButtons
+            categories={categories}
+            buttonSize={'default'}
+            buttonVariant={'default'}
+            className='flex flex-wrap gap-2 md:flex-row'
+          /> */}
+        </FramerDiv>
+        <Projects projects={projects} />
+      </div>
     </section>
   )
 }
