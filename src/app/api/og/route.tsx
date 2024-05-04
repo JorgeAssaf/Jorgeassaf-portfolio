5dcb22159715ab0f59551ccc792834cbaeabcbce
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-03 23:32:34 -0600

    :page_facing_up: update OG route styling to use full height and width

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -15,7 +15,7 @@ export function GET(req: Request) {
     return new ImageResponse(
       (
         <div
-          tw='flex size-full flex-col items-center justify-center'
+          tw='flex h-full w-full flex-col items-center justify-center'
           style={{
             color: mode === 'dark' ? '#fff' : '#000',
             background: mode === 'dark' ? '#09090b' : '#ffffff',
