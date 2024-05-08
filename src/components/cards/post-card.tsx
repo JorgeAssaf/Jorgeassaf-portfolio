e832fd75392e2ca0d02c084eb832ad6ebb0824f2
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-08 08:48:47 -0600

    :sparkles:  Add date formatting using Formaters class in PostCard component

--- src/components/cards/post-card.tsx
+++ src/components/cards/post-card.tsx
@@ -2,10 +2,9 @@
 
 import Link from 'next/link'
 import { FADE_DOWN_ANIMATION_VARIANTS } from '@/constans'
+import { Formaters } from '@/helpers/formaters'
 import type { Post } from 'contentlayer/generated'
 import { m } from 'framer-motion'
-
-import { formatDate } from '@/lib/utils'
 
 import { Badge } from '../ui/badge'
 import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
@@ -27,7 +26,7 @@ export const PostCard = ({ post }: { post: Post }) => {
           </Link>
           <div className='mt-0 md:mt-2'>
             <div className='flex flex-wrap gap-4'>
-              <span>{formatDate(post.date)}</span>
+              <span>{Formaters.formatDateTime(post.date, 'LLLL Y')}</span>
               <div className='flex flex-wrap gap-3'>
                 {post.categories.map((category) => (
                   <Badge
