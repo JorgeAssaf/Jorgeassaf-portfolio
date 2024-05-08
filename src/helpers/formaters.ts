3b8aa8e95160d38cf87ab413875d40415461d1c4
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-08 08:59:15 -0600

    :sparkles: Formaters class to include capitalizeFirstLetter method

--- src/helpers/formaters.ts
+++ src/helpers/formaters.ts
@@ -5,4 +5,9 @@ export class Formaters {
   static formatDateTime(date: Date | string | number, Pattern = 'PPPP'): string {
     return format(new Date(date), Pattern)
   }
+  static capitalizeFirstLetter(string: string | string[]): string {
+    if (string.length === 0) return ''
+    if (Array.isArray(string)) string = string.join('')
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
 }
