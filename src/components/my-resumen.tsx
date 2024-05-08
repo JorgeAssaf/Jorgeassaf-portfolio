bacd78a86199039f705f10c6ccf7d5d6f63330bd
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-08 10:42:29 -0600

    :sparkles: MyResumen component to use new button

--- src/components/my-resumen.tsx
+++ src/components/my-resumen.tsx
@@ -7,22 +7,32 @@ import { ArrowDown } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 
-import { buttonVariants } from './ui/button'
+import { buttonVariants, type ButtonProps } from './ui/button'
 
-export const MyResumen = () => {
+export const MyResumen = ({
+  className,
+  variant = 'default',
+  size,
+}: {
+  className?: string
+  variant?: ButtonProps['variant']
+  size?: ButtonProps['size']
+}) => {
   const [isHovered, setHovered] = useState(false)
   return (
     <Link
-      href='/resume.pdf'
-      aria-label='Download my resume'
+      href='https://drive.google.com/file/d/1KGmCNQLKOSRosglp8x-hNxsHgeHxkGPr/view?usp=sharing'
+      arial-label='Download Resume'
+      target='_blank'
+      rel='noopener noreferrer'
       onMouseEnter={() => setHovered(true)}
       onTouchStart={() => setHovered(true)}
       onTouchEnd={() => setHovered(false)}
       onMouseLeave={() => setHovered(false)}
       download
       className={cn(
-        buttonVariants({ variant: 'default' }),
-        'flex h-0 items-center rounded-full bg-primary px-2.5 py-5 ',
+        buttonVariants({ variant: variant, size: size, className }),
+        'flex h-0 items-center rounded-full px-2.5 py-5 ',
       )}
     >
       <ArrowDown size={20} aria-hidden='true' />
