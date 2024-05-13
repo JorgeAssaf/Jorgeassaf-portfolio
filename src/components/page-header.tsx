15cc1087684a37831d91969c0e39c6372590f33d
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-13 16:48:41 -0600

    :recycle: Refactor PageHeader component to improve title and description rendering

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -41,26 +41,50 @@ export const PageHeader = ({
           page ? 'my-10' : 'my-20',
         )}
       >
-        <m.h1
-          variants={FADE_DOWN_ANIMATION_VARIANTS}
-          className={cn(
-            'flex flex-col justify-center text-3xl font-bold tracking-tight md:text-5xl ',
-            !description && 'flex flex-row',
-          )}
-        >
-          {title}
-        </m.h1>
-
-        {description ? (
+        {page ? (
+          <m.h1
+            variants={FADE_DOWN_ANIMATION_VARIANTS}
+            className={cn(
+              'flex flex-col justify-center text-3xl font-bold tracking-tight md:text-5xl ',
+              !description && 'flex flex-row',
+            )}
+          >
+            {title}
+          </m.h1>
+        ) : (
           <m.h2
             variants={FADE_DOWN_ANIMATION_VARIANTS}
             className={cn(
-              'mt-3 line-clamp-2 text-lg text-muted-foreground md:text-xl  ',
-              !description && 'hidden',
+              'flex flex-col justify-center text-3xl font-bold tracking-tight md:text-5xl ',
+              !description && 'flex flex-row',
             )}
           >
-            {description}
+            {title}
           </m.h2>
+        )}
+
+        {description ? (
+          page ? (
+            <m.h2
+              variants={FADE_DOWN_ANIMATION_VARIANTS}
+              className={cn(
+                'mt-3 line-clamp-2 text-lg text-muted-foreground md:text-xl  ',
+                !description && 'hidden',
+              )}
+            >
+              {description}
+            </m.h2>
+          ) : (
+            <m.p
+              variants={FADE_DOWN_ANIMATION_VARIANTS}
+              className={cn(
+                'mt-3 line-clamp-2 text-lg text-muted-foreground md:text-xl',
+                !description && 'hidden',
+              )}
+            >
+              {description}
+            </m.p>
+          )
         ) : null}
       </div>
     </m.div>
