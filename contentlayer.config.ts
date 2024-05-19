1333a30749130dbb656fa275c4a6b192f63f806f
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-18 22:48:51 -0600

    :wrench:  Add rehype plugins for autolinking headings and generating table of contents

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -4,6 +4,23 @@ import {
   defineNestedType,
   makeSource,
 } from 'contentlayer/source-files'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug'
+import remarkToc from 'remark-toc'
+
+const AuthorLinks = defineNestedType(() => ({
+  name: 'AuthorLinks',
+  fields: {
+    name: {
+      type: 'string',
+      required: true,
+    },
+    url: {
+      type: 'string',
+      required: true,
+    },
+  },
+}))
 
 const Author = defineNestedType(() => ({
   name: 'Author',
@@ -11,6 +28,11 @@ const Author = defineNestedType(() => ({
     name: {
       type: 'string',
       default: 'Anonymous',
+    },
+    links: {
+      type: 'list',
+      of: AuthorLinks,
+      required: true,
     },
     image: {
       type: 'string',
@@ -25,6 +47,8 @@ export const Post = defineDocumentType(() => ({
   contentType: 'mdx',
   fields: {
     title: { type: 'string', required: true },
+    originalUrl: { type: 'string' },
+    mainImage: { type: 'string' },
     date: { type: 'date', required: true },
     author: { type: 'nested', of: Author, required: true },
     categories: { type: 'list', of: { type: 'string' }, required: true },
@@ -35,8 +59,36 @@ export const Post = defineDocumentType(() => ({
       resolve: (post) => `${post._raw.flattenedPath.split('/').pop()}`,
     },
     slugAsParams: {
-      type: "string",
-      resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
+      type: 'string',
+      resolve: (doc) => doc._raw.flattenedPath.split('/').slice(1).join('/'),
+    },
+    toc: {
+      type: 'json',
+      resolve: (doc) => {
+        const regulrExp = /\n(?<flag>#{1,6})\s+(?<content>.+)/g
+
+        const headings = Array.from(doc.body.raw.matchAll(regulrExp)).map(
+          ({ groups }) => {
+            const flag = groups?.flag
+            const content = groups?.content
+
+            return {
+              level:
+                flag?.length == 1
+                  ? 'one'
+                  : flag?.length == 2
+                    ? 'two'
+                    : flag?.length == 3
+                      ? 'three'
+                      : 'four',
+              text: content,
+              slug: content ? content : undefined,
+            }
+          },
+        )
+
+        return headings
+      },
     },
   },
 }))
@@ -45,6 +97,7 @@ export default makeSource({
   contentDirPath: './src/content',
   documentTypes: [Post],
   mdx: {
+    remarkPlugins: [remarkToc],
     rehypePlugins: [
       [
         rehypeShiki as never,
@@ -53,7 +106,20 @@ export default makeSource({
             dark: 'catppuccin-frappe',
             light: 'catppuccin-latte',
           },
+
+          grid: false,
           defaultTheme: 'dark',
+        },
+      ],
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          properties: {
+            ariaHidden: true,
+            tabIndex: -1,
+          },
         },
       ],
     ],
