4c97b4b41db910e5af9a73ba2078b4328cb8d686
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-18 22:57:52 -0600

    :sparkles: Feat: Add pagination buttons component

---
+++ src/components/ui/pagination-buttons.tsx
@@ -0,0 +1,84 @@
+import { useMemo } from 'react'
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronsLeftIcon,
+  ChevronsRightIcon,
+} from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+
+import { Button } from './button'
+
+interface PaginationButtonsProps extends React.HTMLAttributes<HTMLDivElement> {
+  page: number
+  perPage: number
+  totalPage: number
+}
+
+export const PaginationButtons = ({
+  className,
+  page,
+  perPage,
+  totalPage,
+}: PaginationButtonsProps) => {
+  const delta = 1 + 2
+  const paginationRange = useMemo(() => {
+    const range = [] as (number | '...')[]
+
+    for (
+      let i = Math.max(2, Number(page) - delta);
+      i <= Math.min(totalPage - 1, Number(page) + delta);
+      i++
+    ) {
+      range.push(i)
+    }
+
+    if (Number(page) - delta > 2) {
+      range.unshift('...')
+    }
+    if (Number(page) + delta < totalPage - 1) {
+      range.push('...')
+    }
+    range.unshift(1)
+
+    if (totalPage !== 1) {
+      range.push(totalPage)
+    }
+
+    return range
+  }, [delta, page, totalPage])
+
+  return (
+    <div className={cn('flex items-center justify-center gap-2', className)}>
+      <Button variant={'outline'} size={'icon'} disabled={page === 1}>
+        <ChevronsLeftIcon className='size-4' aria-hidden='true' />
+      </Button>
+      <Button variant={'outline'} size={'icon'} disabled={page === 1}>
+        <ChevronLeftIcon className='size-4' aria-hidden='true' />
+      </Button>
+
+      {paginationRange.map((item, i) => {
+        if (item === '...') {
+          return (
+            <span key={i} className='px-2'>
+              ...
+            </span>
+          )
+        }
+
+        return (
+          <Button key={item} variant={page === item ? 'default' : 'outline'}>
+            {item}
+          </Button>
+        )
+      })}
+      <Button variant={'outline'} size={'icon'} disabled={page === 1}>
+        <ChevronRightIcon className='size-4' aria-hidden='true' />
+      </Button>
+      <Button variant={'outline'} size={'icon'} disabled={page === 1}>
+        <ChevronsRightIcon className='size-4' aria-hidden='true' />
+      </Button>
+    </div>
+  )
+}
