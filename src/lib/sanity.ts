2e2b631cdc3390ccc27001ccf55e0b36a7da1dbc
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-18 22:52:57 -0600

    :recycle: Refactor Sanity client initialization to use default API version if not provided

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,11 +2,11 @@ import { createClient, type SanityClient } from 'next-sanity'
 
 const projectId = process.env.SANITY_PROJECT_ID
 const dataset = process.env.SANITY_DATASET
-const apiVersion = process.env.SANITY_API_VERSION
+const apiVersion = process.env.SANITY_API_VERSION ?? '2021-03-25'
 
-export const client: SanityClient = createClient({
+export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: true,
-})
+}) satisfies SanityClient
