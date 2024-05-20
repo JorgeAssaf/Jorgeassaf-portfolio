8afd1966c28a6532e5b756096ea7cfa20cde12b3
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-20 11:53:08 -0600

    :memo: Update next.config.js to include additional CDN hostnames

--- next.config.js
+++ next.config.js
@@ -10,6 +10,18 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'cdn.sanity.io',
       },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pub-f17d057acab94eff8169231fde6b2dd0.r2.dev',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
     ],
   },
   transpilePackages: ['three', '@react-three/drei', '@react-three/fiber'],
