ff18fc94b4e7be83f52c2a548161d0724bd5fd1e
Jorge Assaf <jorgeassaf160@gmail.com>
2024-05-27 15:13:53 -0600

    ♻️ Refactor toc

--- src/components/toc.tsx
+++ src/components/toc.tsx
@@ -2,18 +2,30 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 'use client'
 
+import { useEffect, useState } from 'react'
+
 import { cn, slugify } from '@/lib/utils'
 import type { TocItem } from '@/app/(home)/blog/[...slug]/page'
 
 export const Toc = ({ toc }: { toc: TocItem[] }) => {
-  if (typeof window === 'undefined') return null
-  const hash = window.location.hash
+  const [hash, setHash] = useState('')
+  useEffect(() => {
+    const getHash = () => {
+      if (typeof window === 'undefined') return null
+      const hash = window.location.hash
+      setHash(hash)
+    }
+
+    window.addEventListener('hashchange', getHash)
+
+    return () => window.removeEventListener('hashchange', getHash)
+  }, [])
+
   return (
     <div className='relative mx-auto my-0 rounded-xl border border-solid bg-card/60 px-4 py-3 font-medium'>
       <h3 className='py-2 text-xl font-semibold'>Table of Contents</h3>
       <ul className='list-none'>
         {toc.map((heading: TocItem) => {
-          console.log(`#${slugify(heading.slug)}`, hash)
           return (
             <li
               key={`#${heading.slug}`}
