fbc2086bc407339073530487f86047562f60a1a5
Jorge Assaf <jorgeassaf160@gmail.com>
2025-01-02 20:21:18 -0600

    ✏️ Fix ThemeProvider import for improved type handling

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import type { ThemeProviderProps } from 'next-themes/dist/types'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
